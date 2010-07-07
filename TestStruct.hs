92e97926e802a60f10d1f2ef58e8b2f05e31585d
Timothy Carstens <carstens@math.utah.edu>
2010-07-07 02:16:47 -0600

    Introduced a new function (init2) to TestStruct.hs that shows how to perform a sequence of injections to initialize a datatype.

--- TestStruct.hs
+++ TestStruct.hs
@@ -95,6 +95,14 @@ init = nestMemoryRegion $ \sr ->
 	-- This below causes failure *hooray!*
 	-- inj_InterruptGate_8 r12 rax sr
 
+init2 = nestMemoryRegion $ \sr1 ->
+     do nestMemoryRegion $ \sr2 ->
+	     do newInterruptGate rax []
+		lift $ pop r10
+		inj_InterruptGate_0 r10 rax sr2
+	lift $ pop r10
+	inj_InterruptGate_8 r10 rax sr1
+
 {-
 testInjector =
      do nestMemoryRegion $ \sr -> evaluateTypes $
