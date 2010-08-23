4b0cdb8f3576337be48782a9f23fedcd59480efc
Timothy Carstens <carstens@math.utah.edu>
2010-08-22 18:33:46 -0600

    The Potential Standard Library is now under LGPLv3.  The Potential Compiler is now under GPLv3.  Introduced cabal files for both.

--- Language/Potential/THLiftDecls.hs
+++ Language/Potential/THLiftDecls.hs
@@ -1,3 +1,20 @@
+{-
+  Copyright 2010 Timothy Carstens    carstens@math.utah.edu
+
+  This file is part of the Potential Standard Library.
+
+    The Potential Standard Library is free software: you can redistribute it
+    and/or modify it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, version 3 of the License.
+
+    The Potential Compiler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+-}
 {-# LANGUAGE
 	TemplateHaskell,
 	TypeSynonymInstances #-}
