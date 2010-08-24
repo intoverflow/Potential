3842798d6ce1b4034c13c9b8fcf9a9e8210ca894
Timothy Carstens <carstens@math.utah.edu>
2010-08-24 10:09:28 -0600

    Added Tests.Multiboot.hs, which beings modeling the GRUB multiboot format.

--- tests.sh
+++ tests.sh
@@ -4,9 +4,10 @@
 
 PC=$HOME/bin/pc
 
-time $PC compile --outdir=Tests --outfile=TestArray.S Tests.TestArray
-time $PC compile --outdir=Tests --outfile=TestCode.S Tests.TestCode
-time $PC compile --outdir=Tests --outfile=TestStruct.S Tests.TestStruct
+time $PC compile --outdir=Tests --outfile=testArray.S Tests.TestArray
+time $PC compile --outdir=Tests --outfile=testCode.S Tests.TestCode
+time $PC compile --outdir=Tests --outfile=testStruct.S Tests.TestStruct
+time $PC compile --outdir=Tests --outfile=multiboot.S Tests.Multiboot
 
 time $PC compile --outdir=Tests/C/Add --outfile=add2.S Tests.C.Add.Add2
 time $PC compile --outdir=Tests/C/IntGate --outfile=changeDpl2.S Tests.C.IntGate.ChangeDpl2
